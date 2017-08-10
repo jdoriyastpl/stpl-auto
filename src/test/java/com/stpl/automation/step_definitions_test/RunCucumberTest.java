34f10cf792fcb7e2fa5809bc9be2a2d8d1b41d52
jdoriyastpl <jdoriya@sarvika.com>
2017-08-10 17:26:01 +0530

    update smoke feature for Parex Usa

--- src/test/java/com/stpl/automation/step_definitions_test/RunCucumberTest.java
+++ src/test/java/com/stpl/automation/step_definitions_test/RunCucumberTest.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
  *
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", plugin = { "pretty",
+@CucumberOptions(features = "classpath:features/checkout_smoke_parex_usa.feature", plugin = { "pretty",
 		"html:target/cucumber-html-report" }, tags = {})
 public class RunCucumberTest {
 
--- src/test/resources/features/smoke.feature
+++
@@ -1,8 +0,0 @@
-Feature: Test Store
-  I want to test store
-
-  Scenario: Test Store Landing Page
-    Given I have Store url
-    When I click on CategoryName
-    Then I navigate to Category Page
-    And I add an item into Cart
